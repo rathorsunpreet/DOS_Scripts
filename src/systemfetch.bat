6002c4639b5f026b84110523f7f2fcc784ebffc8
Sunpreet Singh Rathor <rathorsunpreet@gmail.com>
2024-05-26 17:46:12 -0400

    Rectified comments in systemfetch

--- src/systemfetch.bat
+++ src/systemfetch.bat
@@ -2,7 +2,7 @@
 Rem Author: Sunpreet Singh Rathor
 Rem Batch script to display system info like neofetch
 
-Rem Display username@hostname
+Rem Display hostname\username
 for /f "tokens=* skip=1" %%i in (
   'wmic computersystem get username ^| findstr "."'
 ) do echo %%i
@@ -11,22 +11,22 @@ echo ------------------------------------
 echo[
 
 
-Rem Run systeminfo command, pipe the info to findstr then iterate over the output and print it
+Rem Display OS Name, System Boot Time, OS Version, System Architecture (Type) and Total Physical Memory
 for /f "tokens=* delims=: " %%i in (
   'systeminfo ^|  findstr /B /C:"OS Name" /C:"System Boot Time" /C:"OS Version" /C:"System Type" /C:"Total Physical Memory"'
 ) do echo %%i
 
-Rem Run wmic command, iterate over it and print the result
+Rem Display GPU Card Name
 for /f "tokens=* skip=1" %%i in (
   'wmic path win32_VideoController get name ^| findstr "."'
 ) do echo GPU:			   %%i
 
-Rem Run wmic command, iterate over it and print the result
+Rem Display CPU Processor in use
 for /f "tokens=* skip=1" %%i in (
   'wmic cpu get name ^| findstr "."'
 ) do echo CPU:			   %%i
 
-Rem Run wmic command, iterate over it
+Rem Display Total Disk Space in GB
 for /f "tokens=* skip=1" %%i in (
   'wmic diskdrive get size ^| findstr "."'
 ) do set dspace=%%i
@@ -36,15 +36,16 @@ set mb=%kb:~0,-3%
 set gb=%mb:~0,-3%
 echo Total HDD Space:   	   %gb%GB
 
-Rem Run wmic command and iterate over it
+Rem Get Vertical Resolution
 for /f "tokens=* skip=1" %%i in (
   'wmic path win32_VideoController get CurrentVerticalResolution ^| findstr "."'
 ) do set vres=%%i
-Rem Run wmic command and iterate over it
+Rem Get Horizontal Resolution
 for /f "tokens=* skip=1" %%i in (
   'wmic path win32_VideoController get CurrentHorizontalResolution ^| findstr "."'
 ) do set hres=%%i
 Rem Remove all whitespace
 set hres=%hres: =%
 set vres=%vres: =%
+Rem Display Horizontal x Vertical Resolution
 echo Resolution: 		   %hres% x %vres%
